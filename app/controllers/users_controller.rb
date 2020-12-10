ffc6fae9cae5e1cc1f8b049e5545a80ebcf7caf8
vagrant <vagrant@localhost.localdomain>
2020-12-10 11:39:00 +0000

    [test]change body_image to profile_image on progress_logs index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,35 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @recipes = Recipe.where(creation_status: "作成完了",user_id: current_user.id)
+    @today_intake = current_user.intake_dates.find_by(recorded_on: Date.today)
+
+    @daily_total_calorie = 0
+    @daily_total_protein = 0
+    @daily_total_fat = 0
+    @daily_total_carbohydrate = 0
+
+    if @today_intake.present?
+      @today_intake.intake_recipes.each do |ir|
+        @daily_total_calorie += (ir.recipe.total_calorie * ir.number).round
+        @daily_total_protein += (ir.recipe.protein_content * ir.number).round
+        @daily_total_fat += (ir.recipe.fat_content * ir.number).round
+        @daily_total_carbohydrate += (ir.recipe.carbohydrate_content * ir.number).round
+      end
+
+      @today_intake.intake_ingredients.each do |ii|
+        @daily_total_calorie += (ii.ingredient.total_calorie * ii.weight/100).round
+        @daily_total_protein += (ii.ingredient.protein_content * ii.weight/100).round
+        @daily_total_fat += (ii.ingredient.fat_content * ii.weight/100).round
+        @daily_total_carbohydrate += (ii.ingredient.carbohydrate_content * ii.weight/100).round
+      end
+    end
+
+    unless current_user.daily_target_calorie.blank?
+      @daily_protein_content = (current_user.daily_target_calorie * current_user.protein_intake_ratio/100) / 4
+      @daily_fat_content = (current_user.daily_target_calorie * current_user.fat_intake_ratio/100) / 9
+      @daily_carbohydrate_content = (current_user.daily_target_calorie * current_user.carbohydrate_intake_ratio/100) / 4
+    end
   end
 
   def show
